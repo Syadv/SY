07316596b8515d657be1796af444a13e6b7d3901
Sidhant Yadav <1742sidhant@gmail.com>
2024-07-10 20:36:10 +1000

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/SY/', // Set this to your GitHub repository name
   plugins: [react()],
   assetsInclude: ['**/*.glb', '**/*.gltf'],
-})
+});
